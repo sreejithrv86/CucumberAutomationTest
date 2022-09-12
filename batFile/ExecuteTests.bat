e019c3b33772fc38e5b80cecdf1ab909f6baaf17
HP <sreejithrv86@gmail.com>
2022-09-12 08:01:15 +0530

    New Juice

--- batFile/ExecuteTests.bat
+++ batFile/ExecuteTests.bat
@@ -1,4 +1,4 @@
 echo on
 cd C:\Users\HP\git\CucumberAutomationTest\target
-java -jar CucumberAutomationTest-0.0.2.jar classpath:features -n "Login Functionality"
+java -jar -Dbrowsername=chrome -Dapplicationname=phptravels -Denvname=qa CucumberAutomationTest-0.0.2.jar classpath:features -n "Login Functionality"
 Pause
