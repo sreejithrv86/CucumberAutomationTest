10b3f2857a8a348be800360cffe900f6958b798f
HP <sreejithrv86@gmail.com>
2022-10-01 11:15:01 +0530

    Solomon

--- src/main/java/com/org/generic/Utility/TestContext.java
+++ src/main/java/com/org/generic/Utility/TestContext.java
@@ -1,6 +1,6 @@
 package com.org.generic.Utility;
 
-import com.org.generic.Utility.DriverFactory;
+import com.org.generic.Report.ExtentTestManager;
 import com.org.page.PomManager.PageObjectManager;
 
 public class TestContext {
@@ -8,11 +8,13 @@ public class TestContext {
     private final PageObjectManager pageObjectManager;
     private final ScenarioFactory scenarioContext;
     private final DriverFactory driverManager;
+    private final ExtentTestManager extTestManager;
     
     public TestContext() {
         pageObjectManager = new PageObjectManager(DriverFactory.getInstance().getDriver());
         scenarioContext = ScenarioFactory.getInstance();
         driverManager = DriverFactory.getInstance();
+        extTestManager = ExtentTestManager.getInstance();
     }
 
      public PageObjectManager getPageObjectManager() {
@@ -25,5 +27,9 @@ public class TestContext {
     
     public DriverFactory getDriverManager() {
         return driverManager;
+    }
+    
+    public ExtentTestManager getExtentTestManager() {
+        return extTestManager;
     }
 }
