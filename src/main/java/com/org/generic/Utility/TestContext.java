c648fadcba126899ff2cbda5d3d94c5855be407a
HP <sreejithrv86@gmail.com>
2022-10-09 17:45:25 +0530

    Shoolam Kuddi

--- src/main/java/com/org/generic/Utility/TestContext.java
+++ src/main/java/com/org/generic/Utility/TestContext.java
@@ -7,15 +7,11 @@ public class TestContext {
 	private final PageObjectManager pageObjectManager;
 	private final DataFactory dataContext;
 	private final DriverFactory driverManager;
-	private final ScenarioFactory scnFactory;
-	private final FileReaderManager fileManager;
 
 	public TestContext() {
 		pageObjectManager = new PageObjectManager(DriverFactory.getInstance().getDriver());
 		dataContext = DataFactory.getInstance();
 		driverManager = DriverFactory.getInstance();
-		scnFactory = ScenarioFactory.getInstance();
-		fileManager= FileReaderManager.getInstance();
 	}
 
 	public PageObjectManager getPageObjectManager() {
@@ -28,14 +24,6 @@ public class TestContext {
 
 	public DriverFactory getDriverManager() {
 		return driverManager;
-	}
-	
-	public ScenarioFactory getScenariorioManager() {
-		return scnFactory;
-	}
-	
-	public FileReaderManager getFileManager() {
-		return fileManager;
 	}
 
 }
